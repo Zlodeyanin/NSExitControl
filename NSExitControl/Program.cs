aa0fcd1f5ca7d1adbd28b23b692dee153e1a8d67
Zlodeyanin <evgeniy_zavarnitsin@mail.ru>
2025-02-01 10:19:02 +0300

    Добавьте файлы проекта.

---
+++ NSExitControl/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NSExitControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string exitCommand = "exit";
+            string userInput = "";
+
+            while (userInput != exitCommand)
+            {
+                Console.WriteLine("Цикл работает, введите exit для выхода: ");
+                userInput = Console.ReadLine();
+            }
+            Console.WriteLine("Вы вышли из цикла.");
+            Console.ReadKey();
+        }
+    }
+}
